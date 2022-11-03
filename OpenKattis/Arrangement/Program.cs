404df19cfc5e240756e0987182bbd13c0c48e85d
Simon Grey <S.Grey@hull.ac.uk>
2022-11-03 10:19:39 +0000

    Completed Arrangement Problem

---
+++ OpenKattis/Arrangement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace myNamespace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int numberOfRooms = int.Parse(Console.ReadLine());
+            int numberOfTeams = int.Parse(Console.ReadLine());
+
+            int teamsPerRoom = numberOfTeams / numberOfRooms;
+            int remainder = numberOfTeams % numberOfRooms;
+
+            for (int i = 0; i < numberOfRooms; i++)
+            {
+                for (int j = 0; j < teamsPerRoom; j++)
+                {
+                    Console.Write("*");
+                }
+                if (remainder > 0)
+                {
+                    Console.Write("*");
+                    remainder--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
