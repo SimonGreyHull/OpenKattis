a94234f48ece685046ca9ad7bceffdcd378a5515
Simon Grey <S.Grey@hull.ac.uk>
2022-11-03 10:40:57 +0000

    Completed Shattered Cake

---
+++ OpenKattis/Shattered Cake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace myNamespace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+
+            int numberOfPieces = int.Parse(Console.ReadLine());
+
+            int totalArea = 0;
+
+            for(int i = 0; i < numberOfPieces; i++)
+            {
+                string str = Console.ReadLine();
+                string[] dimensionStrings = str.Split(" ");
+
+                int pieceWidth = int.Parse(dimensionStrings[0]);
+                int pieceHeight = int.Parse(dimensionStrings[1]);
+
+                totalArea = totalArea + pieceWidth * pieceHeight;
+            }
+
+            int length = totalArea / width;
+
+            Console.WriteLine(length);
+        }
+    }
+
+}
