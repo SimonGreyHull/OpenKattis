455c3a4598ef192754d385375ed686f1ae0c17b7
Simon Grey <S.Grey@hull.ac.uk>
2022-11-03 10:32:02 +0000

    Completed Divvying Up

---
+++ OpenKattis/Divvying Up/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace myNamespace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            str = Console.ReadLine();
+
+            string[] values = str.Split(" ");
+
+            int totalWinnings = 0;
+            int numberOfComptetions = values.Length;
+        
+            for(int i = 0; i < numberOfComptetions; i++)
+            {
+                totalWinnings = totalWinnings + int.Parse(values[i]);
+            }
+
+            if(totalWinnings % 3 == 0)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }    
+        }
+    }
+}
